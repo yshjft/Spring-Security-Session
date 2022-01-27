e93908d8aac2822987923d6fd649b551b3d9b5e0
hong yuseok <jericoh@naver.com>
2022-01-27 11:35:32 +0900

    Basic Setting: add log interceptor

---
+++ src/main/java/com/springSecurity/SpringSecuritySession/config/WebConfig.java
@@ -0,0 +1,16 @@
+package com.springSecurity.SpringSecuritySession.config;
+
+import com.springSecurity.SpringSecuritySession.web.interceptor.LogInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LogInterceptor())
+                .order(1)
+                .addPathPatterns("/**");
+    }
+}
---
+++ src/main/java/com/springSecurity/SpringSecuritySession/web/interceptor/LogInterceptor.java
@@ -0,0 +1,39 @@
+package com.springSecurity.SpringSecuritySession.web.interceptor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
+
+@Slf4j
+public class LogInterceptor implements HandlerInterceptor {
+    public static final String LOG_ID="LOG_ID";
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String uuid = UUID.randomUUID().toString();
+        request.setAttribute(LOG_ID, uuid);
+
+        String uri = request.getRequestURI();
+        String method = request.getMethod();
+
+        log.info("[ID: {}] REQUEST {} {}", uuid, method, uri);
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+        String uuid = request.getAttribute(LOG_ID).toString();
+        String uri = request.getRequestURI();
+        String method = request.getMethod();
+
+        log.info("[ID: {}] RESPONSE {} {}", uuid, method, uri);
+    }
+}
